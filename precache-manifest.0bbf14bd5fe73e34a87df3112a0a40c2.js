7053c01182604ecd74fbcaf31e734ec6346a3bed
Colin Young <young.colin86@gmail.com>
2020-09-12 19:57:54 -0700

    Updates

---
+++ precache-manifest.0bbf14bd5fe73e34a87df3112a0a40c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02de8235e31a2d3ce47785eea3bc597d",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "24e995d8b5ad20c1ac94",
+    "url": "/my-app/static/css/main.0b167f0c.chunk.css"
+  },
+  {
+    "revision": "8a61eafb87848e827d75",
+    "url": "/my-app/static/js/2.075201e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.075201e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24e995d8b5ad20c1ac94",
+    "url": "/my-app/static/js/main.373896cc.chunk.js"
+  },
+  {
+    "revision": "82548d45436401cf8742",
+    "url": "/my-app/static/js/runtime-main.4027eff1.js"
+  }
+]);
